bf6ae0610570f0e615efc7a75e4e57bb5b1190e8
Quelynxyz <inuremon@gmail.com>
2021-02-25 23:09:01 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = "jiddoGt5BfvKv5EsGo0KP3YLglgypZ2r"
+	var map = L.mapquest.map('map' , {
+		center: [32.882 , -117.234], 
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+	L.marker([32.882 , -117.234]).addTo(map);
 }
